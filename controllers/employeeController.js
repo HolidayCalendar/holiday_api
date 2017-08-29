35e0fea77f805c7d72ab2d721371ef36647665db
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-08-29 15:39:45 +0200

    adding class for employees controllers

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -14,59 +14,85 @@ class Employee {
     this.response.json(data);
   }
 
+  show() {
+    this.render({message: 'method not implemented'});
+  }
+
   index() {
-    this.render(this.params);
+    this.render({ message: 'method not implemented'});
   }
 
   create() {
-
+    this.render({ message: 'method not implemented'});
   }
 
   update() {
-
+    this.render({ message: 'method not implemented'});
   }
 
-  destroy() {
+  delete() {
+    this.render({ message: 'method not implemented'});
+  }
 
+  static show(req, res) {
+    new this(req, res).show();
+  }
+
+  static index(req, res) {
+    new this(req, res).index();
+  }
+
+  static create(req, res) {
+    new this(req, res).create();
+  }
+
+  static update(req, res) {
+    new this(req, res).update();
+  }
+
+  static delete(req, res) {
+    new this(req, res).delete();
   }
 }
 
-// Display list of all Employees
-exports.employees_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Employees list');
-};
+module.exports = Employee
 
-// Display detail page for a specific Employee
-exports.employee_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Employee detail: ' + req.params.id);
-};
-
-// Display Employee create form on GET
-exports.employee_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Emplyee create GET');
-};
-
-// Handle Emplyee create on POST
-exports.employee_create_post = function(req, res) {
-    new User(req, res).create();
-};
-
-// Display Emplyee delete form on GET
-exports.employee_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Emplyee delete GET');
-};
-
-// Handle Emplyee delete on POST
-exports.employee_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Emplyee delete POST');
-};
-
-// Display Emplyee update form on GET
-exports.employee_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Emplyee update GET');
-};
-
-// Handle Emplyee update on POST
-exports.employee_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Emplyee update POST');
-};
+// // Display list of all Employees
+// exports.employees_list = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Employees list');
+// };
+//
+// // Display detail page for a specific Employee
+// exports.employee_detail = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Employee detail: ' + req.params.id);
+// };
+//
+// // Display Employee create form on GET
+// exports.employee_create_get = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Emplyee create GET');
+// };
+//
+// // Handle Emplyee create on POST
+// exports.employee_create_post = function(req, res) {
+//     new User(req, res).create();
+// };
+//
+// // Display Emplyee delete form on GET
+// exports.employee_delete_get = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Emplyee delete GET');
+// };
+//
+// // Handle Emplyee delete on POST
+// exports.employee_delete_post = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Emplyee delete POST');
+// };
+//
+// // Display Emplyee update form on GET
+// exports.employee_update_get = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Emplyee update GET');
+// };
+//
+// // Handle Emplyee update on POST
+// exports.employee_update_post = function(req, res) {
+//     res.send('NOT IMPLEMENTED: Emplyee update POST');
+// };
--- routes/employee.js
+++ routes/employee.js
@@ -2,30 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 // Require controller module
-var employee_controller = require('../controllers/employeeController');
+var EmployeesController = require('../controllers/employeeController');
 
 /* GET request for creating a Employee.*/
-router.get('/create', employee_controller.employee_create_get);
+// router.get('/create', employee_controller.employee_create_get);
 
 /* POST request for creating Employee. */
-router.post('/create', employee_controller.employee_create_post);
+router.post('/create', EmployeesController.create);
 
 /* GET request to delete Employee. */
-router.get('/:id/delete', employee_controller.employee_delete_get);
+// router.get('/:id/delete', employee_controller.employee_delete_get);
 
 // POST request to delete Employee
-router.post('/:id/delete', employee_controller.employee_delete_post);
+router.post('/:id/delete', EmployeesController.delete);
 
 /* GET request to update Employee. */
-router.get('/:id/update', employee_controller.employee_update_get);
+// router.get('/:id/update', employee_controller.employee_update_get);
 
 // POST request to update Employee
-router.post('/:id/update', employee_controller.employee_update_post);
+router.post('/:id/update', EmployeesController.update);
 
 /* GET request for one Employee. */
-router.get('/:id', employee_controller.employee_detail);
+router.get('/:id', EmployeesController.show);
 
 /* GET request for list of all Employee items. */
-router.get('/', employee_controller.employees_list);
+router.get('/', EmployeesController.index);
 
 module.exports = router;
